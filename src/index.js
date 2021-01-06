fdf2c60f4bd477d34650c7b86f763aa0c469f59e
serchtruj <serchtruj1@gmail.com>
2021-01-05 18:39:09 -0600

    Add apollo provider to app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
