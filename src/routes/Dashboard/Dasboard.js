fbbdd525adf46d6b9b6f23e9cde76f4e2fd3e789
serchtruj <serchtruj1@gmail.com>
2021-01-09 13:32:09 -0600

    Add fc to manage date and details

--- src/routes/Dashboard/Dasboard.js
+++ src/routes/Dashboard/Dasboard.js
@@ -6,11 +6,27 @@ import {GET_ALL_LAUNCHES} from "../../grapql/queries/index"
 const Dasboard = () => {
   const {loading, error, data} = useQuery(GET_ALL_LAUNCHES)
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+  
+  function transformDate(dateString){
+      const getDate = /\d{4}[-]\d{2}[-]\d{2}/.exec(dateString)
+      const splitDate = getDate[0].split("-")
+      return  [splitDate[1], splitDate[2], splitDate[0]].join("/")
+  }
+
   if(loading)return <p>Loading...</p>
   if(error) return <p>Error: {error}</p>
   return <>
+    <h1>Last Launches</h1>
     {data.launchesPast.map(launch => (
-        <Launch launch={launch} key={launch.id}/>
+        <Launch 
+          launch={launch} 
+          key={launch.id} 
+          truncate={truncate} 
+          transformDate={transformDate}
+        />
     ))}
   </>;
 }
