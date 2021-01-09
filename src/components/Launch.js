13e19f38287da809242d64dbcaf9f2f0fde56b29
serchtruj <serchtruj1@gmail.com>
2021-01-09 13:32:43 -0600

    Include fc for manage strings

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,19 +1,22 @@
 import PropTypes from "prop-types"
 import {Link} from "react-router-dom"
 
-function Launch({launch}) {
-    const {id, mission_name} = launch;
+function Launch({launch, truncate, transformDate}) {
+    const {mission_name, details, launch_date_local, id} = launch;
     return (
-        <div >
-            {id}
+        <div>
             {mission_name}
+            {truncate(details, 100)}
+            {transformDate(launch_date_local)}
             <button><Link to={`/mission/${id}`}>See More</Link></button>
         </div>
     )
 }
 
 Launch.propTypes = {
-    launch: PropTypes.object
+    launch: PropTypes.object,
+    truncate: PropTypes.func,
+    transformDate: PropTypes.func
 }
 
 export default Launch
