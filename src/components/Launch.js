37e1cac429ebb5feab151f2cce2ecd2673af8620
serchtruj <serchtruj1@gmail.com>
2021-01-11 16:12:27 -0600

    Add styles to launch

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,22 +1,64 @@
 import PropTypes from "prop-types"
-import {Link} from "react-router-dom"
+import styled from "styled-components"
+import { useHistory } from "react-router-dom";
 
-function Launch({launch, truncate, transformDate}) {
+const Container = styled.div`
+    max-width: 450px;
+    height: 120px;
+    padding: 18px;
+    margin: 10px;
+    border: 2px solid #c3cfd9;
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+
+    &:hover {
+        border: 2px solid black;
+        background-color: #c3cfd9;
+    }
+
+    h3 {
+        padding-bottom: 7px;
+    }
+
+    p {
+        line-height: 1.5;
+    }
+}
+
+`
+const DetailsLaunch = styled.p`
+    padding-top: 7px;
+    align-self: flex-end;
+    font-style: italic;
+`
+
+function Launch({launch, fn}) {
     const {mission_name, details, launch_date_local, id} = launch;
+    const {truncate, transformDate} = fn
+    let history = useHistory()
+
+    function handleClick(){
+        history.push(`/mission/${id}`)
+    }
+
     return (
-        <div>
-            {mission_name}
-            {truncate(details, 100)}
-            {transformDate(launch_date_local)}
-            <button><Link to={`/mission/${id}`}>See More</Link></button>
-        </div>
+        <Container onClick={handleClick}>
+            <h3>{mission_name}</h3>
+            <p>
+                {details?.length ? 
+                truncate(details, 110) : 
+                "Details not available"}
+            </p>
+            <DetailsLaunch>{transformDate(launch_date_local)}</DetailsLaunch>
+        </Container>
     )
 }
 
 Launch.propTypes = {
     launch: PropTypes.object,
-    truncate: PropTypes.func,
-    transformDate: PropTypes.func
+    fn: PropTypes.object,
 }
 
 export default Launch
