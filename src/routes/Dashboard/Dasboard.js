28e65a4b51827437af0f4c21b0dabc1234f20994
serchtruj <serchtruj1@gmail.com>
2021-01-11 19:58:33 -0600

    Refactor fn handle to children

--- src/routes/Dashboard/Dasboard.js
+++ src/routes/Dashboard/Dasboard.js
@@ -12,41 +12,28 @@ const Container = styled.div`
     background-size: cover;
     background-image: url("https://i.pinimg.com/originals/1b/9f/b2/1b9fb29941b62bbdf0901120713e1fc2.jpg");
 `
-
-const Row = styled.div`
+const LastLaunches = styled.div`
   h1 {
     padding-left: 7px;
     color: white;
   }
 `
-
 const Dasboard = () => {
   const {loading, error, data} = useQuery(GET_ALL_LAUNCHES)
-
-  function truncate(str, n) {
-    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-  }
-
-  function transformDate(dateString){
-      const getDate = /^(\d{4}[-]\d{2}[-]\d{2})/.exec(dateString)
-      const splitDate = getDate[0].split("-")
-      return  [splitDate[1], splitDate[2], splitDate[0]].join("/")
-  }
 
   if(loading)return <Loading/>
   if(error) return <ErrorPage error={error}/>
   return(
     <Container>
-      <Row>
+      <LastLaunches>
         <h1>Last Launches 🚀</h1>
         {data.launchesPast.map(launch => (
             <Launch 
               launch={launch} 
-              key={launch.id} 
-              fn={{truncate,transformDate}}
+              key={launch.id}
             />
         ))}
-      </Row>
+      </LastLaunches>
     </Container>
   )
 }
