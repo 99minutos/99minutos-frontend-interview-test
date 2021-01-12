ce5fbe2346ec2611e4c3a932061a3be7607736f6
serchtruj <serchtruj1@gmail.com>
2021-01-11 18:11:00 -0600

    Remove styles and add Error page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
 import {Switch, Route} from "react-router-dom"
 import Dashboard from "./routes/Dashboard/Dasboard"
 import Mission from "./routes/Mission/Mission"
 import Home from "./routes/Home/Home"
+import ErrorPage from "./components/ErrorPage"
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
           <Route exact path="/" component={Home}/>
           <Route path="/dashboard" component={Dashboard}/>
           <Route path="/mission/:missionId" component={Mission}/>
+          <Route component={ErrorPage}/>
         </Switch>
     </div>
   );
