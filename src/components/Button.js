b963b39b6bb3373d7f248769f22b53319943c432
serchtruj <serchtruj1@gmail.com>
2021-01-11 18:50:47 -0600

    Create button component

---
+++ src/components/Button.js
@@ -0,0 +1,35 @@
+import styled from "styled-components"
+import PropTypes from "prop-types"
+
+const Container = styled.button`
+    height: 40px;
+    width: 100px;
+    border: none;
+    background-color: #c3cfd9;
+    color: #293845;
+    font-size: 1em;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    cursor:pointer;
+
+    &:hover {
+        background-color:#293845;
+        color: #c3cfd9
+    }
+    
+    &:focus{
+        outline:0;
+    }
+`
+
+export default function Button ({children, ...rest}){
+    return(
+        <Container {...rest}>
+            {children}
+        </Container>
+    )
+}
+
+Button.propTypes = {
+    children: PropTypes.children
+}
