705bbc0123dbdf04ebded97d9833f1ef9c0017b6
Juan Bojato <jbojato@uninorte.edu.co>
2021-07-01 12:48:52 -0500

    initial layout

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container-fluid">
+        <div class="main-container">
+          <div class="col-sm-4 launches">
+            <div class="row ">Column</div>
+          </div>
+          <div class="col-sm-8 mission">
+            <div class="row imageMission">row 1</div>
+            <div class="row infoMission">row 2</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
