c923d9082c56ecf29987b2410f7d92bbfd0a886d
Juan Bojato <jbojato@uninorte.edu.co>
2021-07-01 17:06:49 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,76 @@
 import "./App.css";
 import Launch from "./components/Launch";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
 function App() {
+  const [info, setInfo] = useState({
+    mission_name: "",
+    details: "",
+    links: "",
+    launch_site: "",
+  });
+  const getInfo = (mission_name, details, links, launch_site) => {
+    setInfo({
+      mission_name: mission_name,
+      details: details,
+      links: links,
+      launch_site: launch_site,
+    });
+  };
+  const getRandomImg = (imgs) => imgs[Math.floor(Math.random() * imgs.length)];
   return (
     <div className="App">
       <div class="container-fluid">
         <div class="main-container">
           <div class="col-sm-5 launches">
             <div class="row ">
-              <h1>Last Launches</h1>
-              <Launch />
+              <h1 id="titleLaunches">Last Launches</h1>
+              <Launch getInfo={getInfo} />
             </div>
           </div>
           <div class="col-sm-7 mission">
-            <div class="row imageMission h-50">row 1</div>
-            <div class="row infoMission h-50">row 2</div>
+            <div class="row imageMission">
+              {" "}
+              {!info.links ? (
+                <img
+                  src="https://dummyimage.com/900x600/696669/fff.png&text=Image"
+                  className="size"
+                  alt="dummy"
+                />
+              ) : info.links.flickr_images.length === 0 ? (
+                <img
+                  src="https://dummyimage.com/900x600/696669/fff.png&text=No+Image"
+                  className="size"
+                  alt="noimage"
+                />
+              ) : (
+                <img
+                  src={info.links.flickr_images[0]}
+                  className="size"
+                  alt={info.mission_name}
+                />
+              )}
+            </div>
+
+            {!info ? (
+              <div> </div>
+            ) : (
+              <div class="row infoMission">
+                <h1>{info.mission_name}</h1>
+                <p>
+                  {info.details === null
+                    ? "There is not description"
+                    : info.details}
+                </p>
+                <Button
+                  href={info.links.video_link}
+                  target="_blank"
+                  variant="danger"
+                >
+                  See More
+                </Button>
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,4 +1,6 @@
 import { useQuery, gql } from "@apollo/client";
+import { useState } from "react";
+
 const GET_LAUNCHES = gql`
   {
     launchesPast(limit: 10) {
@@ -6,14 +8,17 @@ const GET_LAUNCHES = gql`
       details
       links {
         flickr_images
+        video_link
       }
       launch_site {
         site_name_long
       }
+      launch_date_local
     }
   }
 `;
-export default function Launch() {
+export default function Launch({ getInfo }) {
+  const [selected, setSelected] = useState("");
   const { loading, error, data } = useQuery(GET_LAUNCHES);
 
   if (loading) return <p>Loading...</p>;
@@ -23,9 +28,21 @@ export default function Launch() {
     <div className="missionLaunches">
       {data.launchesPast.map(
         ({ mission_name, details, links, launch_site }) => (
-          <div className="w-100" key={mission_name}>
-            <h1>🛰 {mission_name}</h1>
-            <p>{launch_site.site_name_long}</p>
+          <div
+            className="missionParent w-100 "
+            key={mission_name}
+            onClick={() => {
+              getInfo(mission_name, details, links, launch_site);
+              setSelected(mission_name);
+            }}
+            style={{
+              color: selected === mission_name ? "red" : "",
+            }}
+          >
+            <div className="missionItem">
+              <h1>🛰 {mission_name}</h1>
+              <p>{launch_site.site_name_long}</p>
+            </div>
           </div>
         )
       )}
