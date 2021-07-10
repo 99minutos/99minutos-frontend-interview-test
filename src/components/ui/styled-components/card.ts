645a0f3f4885025e2cae5924605c59910c6b3518
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-07-10 14:46:34 -0500

    adding cards and image styled components

---
+++ src/components/ui/styled-components/card.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const CardWrapper = styled.div`
+    box-shadow: 0 2px 1px -1px rgba(0,0,0,.2),0 1px 1px 0 rgba(0,0,0,.14),0 1px 3px 0 rgba(0,0,0,.12);
+    transition: box-shadow 280ms cubic-bezier(0.4, 0, 0.2, 1);
+    display: block;
+    position: relative;
+    padding: 16px;
+    border-radius: 4px;
+    font-family: Roboto,Helvetica Neue,sans-serif;
+    background: #fff;
+    color: rgba(0,0,0,.87);
+    font-weight: 400;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    cursor: pointer;
+`
---
+++ src/components/ui/styled-components/image.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const NavBarLogo = styled.img`
+    display: inline-block;
+    vertical-align: middle;
+    border: 0;
+    line-height: 50px;
+`
+export const ImageThumb = styled.img`
+    display: inline-block;
+    vertical-align: middle;
+    border: 0;
+    line-height: 50px;
+`
+
