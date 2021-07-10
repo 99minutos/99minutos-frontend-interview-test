523ddb2396699cd5885ed833db85eab22faf96e5
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-07-10 14:48:08 -0500

    add sidebar and mainpanel to home

--- src/pages/home/HomeScreen.tsx
+++ src/pages/home/HomeScreen.tsx
@@ -1,18 +1,41 @@
 import React from 'react'
 import styled from "styled-components";
 import { NavBar } from '../../components/shared/navbar/NavBar';
+import { SideBar } from '../../components/shared/sidebar/SideBar';
+import { MainPanel } from '../../components/shared/mainpanel/MainPanel';
 
 
 export const HomeScreen = () => {
     return (
         <HomeWrapper>
+
             <NavBar/>
+
+            <MainWrapper>
+                <SideBar/>
+                <MainPanel/>
+            </MainWrapper>
+
         </HomeWrapper>
     )
 }
 
 const HomeWrapper = styled.div`
     background-color: #2f373e;
+    color: #fff;
     padding-left: 25px;
     padding-right: 25px;
+`
+
+const MainWrapper = styled.div`
+    margin-top: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #ffffffca;
+    border: 1px solid #dadde1;
+    border-radius: 10px;
+    
+
 `
