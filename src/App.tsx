e3fdf1ff71f761607506578bf61a0a8e15231d7a
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-07-12 12:43:13 -0500

    setup graphql client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import { HomeScreen } from './pages/home/HomeScreen';
+// import ApolloClient from 'apollo-boost';
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+	uri:"https://api.spacex.land/graphql/",
+	cache: new InMemoryCache()
+})
 
 const App = () => {
   	return (
-    
-    	<HomeScreen/>
+		<ApolloProvider client={ client }>
+			<HomeScreen/>
+		</ApolloProvider>
 
   	);
 }
