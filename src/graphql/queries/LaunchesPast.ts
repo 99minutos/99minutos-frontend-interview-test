987a2d90e9dd94e7318239c9e203184a8d232891
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-07-12 13:51:11 -0500

    create launchesPast query

---
+++ src/graphql/queries/LaunchesPast.ts
@@ -0,0 +1,34 @@
+import { gql, DocumentNode } from '@apollo/client';
+
+export const GET_LAUNCHES_PAST: DocumentNode = gql`
+{
+    launchesPast(limit: 10) {
+      mission_name
+      launch_date_local
+      launch_site {
+        site_name_long
+      }
+      links {
+        article_link
+        video_link
+      }
+      rocket {
+        rocket_name
+        second_stage {
+          payloads {
+            payload_type
+            payload_mass_kg
+            payload_mass_lbs
+          }
+        }
+      }
+      ships {
+        name
+        home_port
+        image
+      }
+      
+    }
+  }
+  
+`;
