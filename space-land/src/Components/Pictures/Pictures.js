da93d4452231caa481440ce317013a99f6415abf
MiriamNM <miriamnavamucino@gmail.com>
2021-07-22 14:17:23 -0500

    All images are shown when wanting to see more information about the mission

---
+++ space-land/src/Components/Pictures/Pictures.js
@@ -0,0 +1,23 @@
+//import Carousel from 'react-bootstrap/Carousel';
+import Space from '../../assets/space.png';
+import './pictures.css';
+
+const Pictures = (props) => (
+  <>
+    {props.item?.ships[0]?.image?.length >0 ? 
+    (
+      <div className='imageMission'>
+        <img src={props.item.ships[0].image} alt="Mission" />
+      </div>
+    )
+     : 
+     (
+      <div className="withoutImage">
+        <img src={Space} alt='Space'/>
+      </div> 
+      )
+    }
+  </>
+);
+
+export default Pictures;
--- space-land/src/Components/detailsMission/DetailsMission.js
+++ space-land/src/Components/detailsMission/DetailsMission.js
@@ -1,20 +1,15 @@
 import React from 'react';
+import Pictures from '../Pictures/Pictures';
 import './detailsMission.css';
 
 const DetailsMission = (props) => {
     return(
         <>
-            <div className='imageMission'>
-                <img src={props.item.ships[0].image} alt="Mission" />
-            </div>
+            <Pictures item ={props.item}/>
             <div className='contentStoryMission'>
                 <h2>{props.item.mission_name}</h2>
                 <p>{props.item.details}</p>
-                <button><a
-                    href={props.item.links.video_link}
-                    target="_blank"
-                    rel="noreferrer"
-                >
+                <button><a href={props.item.links.video_link}>
                 See More
                 </a></button>
             </div>
